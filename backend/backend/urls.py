3bb7fa8cb99adb9bbfb6f0ad2806a25e0995035d
Maxim Kudeikin <mauricevvs@gmail.com>
2024-11-23 21:39:44 +0300

    update swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,8 +20,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include('home.urls')),
     path('api/chat/', include('chat.urls')),
     path('api/user/', include('users.urls')),
